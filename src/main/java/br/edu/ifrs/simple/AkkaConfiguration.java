eef63829b3c4702ed7fe906da7b0f5630a741a50
Pedro Arthur Duarte (aka JEdi) <pedroarthur.jedi@gmail.com>
2015-09-09 06:30:23 -0300

    Changes indentation to adapt to TeX

--- src/main/java/br/edu/ifrs/simple/AkkaConfiguration.java
+++ src/main/java/br/edu/ifrs/simple/AkkaConfiguration.java
@@ -11,3 +11,4 @@ public class AkkaConfiguration {
         return ActorSystem.create("defaultActorSystem");
     }
 }
+
--- src/main/java/br/edu/ifrs/simple/AkkaHelloWorldApplication.java
+++ src/main/java/br/edu/ifrs/simple/AkkaHelloWorldApplication.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2012-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package br.edu.ifrs.simple;
 
 import akka.actor.ActorRef;
@@ -34,16 +18,17 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class AkkaHelloWorldApplication implements CommandLineRunner {
 
-	@Autowired private ActorSystem actorSystem;
+  @Autowired private ActorSystem actorSystem;
 
-	@Override
-	public void run(String ... args) {
-        ActorRef helloWorldActor = actorSystem.actorOf(Props.create(HelloWorldActor.class));
+  @Override
+  public void run(String ... args) {
+    ActorRef helloWorldActor = actorSystem.actorOf(Props.create(HelloWorldActor.class));
 
-        helloWorldActor.tell(new HelloWorldMessage(),  actorSystem.actorOf(Props.create(HaltingActor.class)));
-	}
+    helloWorldActor.tell(new HelloWorldMessage(),
+        actorSystem.actorOf(Props.create(HaltingActor.class)));
+  }
 
-	public static void main(String[] args) throws Exception {
-	 	SpringApplication.run(AkkaHelloWorldApplication.class, args);
-	}
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(AkkaHelloWorldApplication.class, args);
+  }
 }
